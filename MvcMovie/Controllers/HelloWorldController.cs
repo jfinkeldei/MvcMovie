4b39d32cafb7a92faf2cc7a971a1c7f864ef1935
Joel <Joel@JOEL-PC>
2016-09-29 20:53:57 -0500

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,46 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+        public string Goodbye()
+        {
+            return "Goodbye!";
+        }
+
+        public string HelloWorld()
+        {
+            return "Hello";
+        }
+
+        public string ZeroToNine()
+        {
+            string result = "";
+            for(int i = 0; i < 10; i++)
+            {
+                result += i;
+            }
+
+            return result;
+        }
+    }
+}
